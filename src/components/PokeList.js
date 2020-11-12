1cc3a5a6f7d25fcbfc623589289141bca40bf7d8
martreyz <martreyz@gmail.com>
2020-11-12 10:26:14 +0100

    Add styles and fix render of each card

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,11 +2,14 @@ import Pokemon from "./Pokemon";
 import "../stylesheets/PokeList.scss";
 
 const PokeList = (props) => {
-  return (
-    <ul className="main__list">
-      <Pokemon data={props.data} />
-    </ul>
-  );
+  const cards = props.data.map((pokemon) => {
+    const types = pokemon.types.map((type) => {
+      return <li className="main__list-typesItem">{type}</li>;
+    });
+    return <Pokemon name={pokemon.name} url={pokemon.url} types={types} />;
+  });
+
+  return <ul className="main__list">{cards}</ul>;
 };
 
 export default PokeList;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,24 +1,18 @@
 import "../stylesheets/Pokemon.scss";
 
 const Pokemon = (props) => {
-  const cards = props.data.map((pokemon) => {
-    const types = pokemon.types.map((type) => {
-      return <li className="main__list-typesItem">{type}</li>;
-    });
-    return (
-      <li className="main__list-item">
-        <img
-          className="main__list-img"
-          title={"Imagen de " + pokemon.name}
-          src={pokemon.url}
-          alt={pokemon.name}
-        />
-        <h2 className="main__list-title">{pokemon.name}</h2>
-        <ul className="main__list-types">{types}</ul>
-      </li>
-    );
-  });
-  return cards;
+  return (
+    <li className="main__list-item">
+      <img
+        className="main__list-img"
+        title={"Imagen de " + props.name}
+        src={props.url}
+        alt={props.name}
+      />
+      <h2 className="main__list-title">{props.name}</h2>
+      <ul className="main__list-types">{props.types}</ul>
+    </li>
+  );
 };
 
 export default Pokemon;
